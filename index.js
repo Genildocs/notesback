9b19512be16f1a99859645e8414f58f9a5c1ed72
genildocs <genildocs@gmail.com>
2023-12-22 23:20:22 -0300

    commit

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors());
 
 let notes = [
   {
     id: 1,
-    title: "Title",
-    content: "HTML is easy",
+    content: 'HTML is easy',
     date: new Date().toISOString(),
     important: Math.random() < 0.5,
   },
   {
     id: 2,
-    title: "Title",
-    content: "Browser can execute only Javascript",
-    date: new Date().toISOString(),
-    important: Math.random() < 0.5,
-  },
-  {
-    id: 3,
-    title: "Title",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: new Date().toISOString(),
-    important: Math.random() < 0.5,
-  },
-  {
-    id: 4,
-    title: "Title",
-    content: "Node.js is a single-threaded application",
+    content: 'Browser can execute only Javascript',
     date: new Date().toISOString(),
     important: Math.random() < 0.5,
   },
@@ -42,16 +26,15 @@ const generateId = () => {
   return maxId + 1;
 };
 
-app.post("/api/notes", (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body;
   if (!body.content) {
     return response.status(400).json({
-      error: "content missing",
+      error: 'content missing',
     });
   }
 
   const note = {
-    title: body.title || "Title",
     content: body.content,
     important: body.important || false,
     date: new Date().toISOString(),
@@ -62,15 +45,15 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-app.get("/", (req, res) => {
-  res.send("<h1>Notes</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>Notes</h1>');
 });
 
-app.get("/api/notes", (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   const note = notes.find((note) => note.id === id);
   if (note) {
@@ -81,14 +64,14 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
   response.status(204).end();
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+  response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
