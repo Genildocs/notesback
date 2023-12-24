27c65172b69e294eb124ca9427c3206bc881ed30
genildocs <genildocs@gmail.com>
2023-12-24 03:01:04 -0300

    commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
-const notesRouter = require("express").Router();
 const User = require("../models/user");
 
 usersRouter.post("/", async (request, response) => {
@@ -23,24 +22,6 @@ usersRouter.post("/", async (request, response) => {
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({});
   response.json(users);
-});
-
-notesRouter.post("/", async (request, response) => {
-  const body = request.body;
-
-  const user = await User.findById(body.userId);
-
-  const note = new Note({
-    content: body.content,
-    important: body.important === undefined ? false : body.important,
-    user: user.id,
-  });
-
-  const savedNote = await note.save();
-  user.notes = user.notes.concat(savedNote._id);
-  await user.save();
-
-  response.json(savedNote);
 });
 
 module.exports = usersRouter;
