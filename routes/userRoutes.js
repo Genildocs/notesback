ad964e56dca364347fe6fd3b376bf5bb572f604f
genildocs <genildocs@gmail.com>
2025-01-10 01:13:53 -0300

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
-router.route('/').get(userController.getUsers);
+router.route('/').get(authController.protectRouter, userController.getUsers);
 
 module.exports = router;
